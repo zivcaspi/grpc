0078f42565e96d393b9d7da60b38a4781207f8c9
Ziv Caspi <zivc@microsoft.com>
2019-01-20 10:24:03 +0200

    Genesis -- run service, then run client, and tell the client what port the service is on

---
+++ client/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Grpc.Core;
+using Contract;
+
+namespace client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Which port? ");
+            var port = int.Parse(Console.ReadLine());
+            var channel = new Channel($"localhost:{port}", ChannelCredentials.Insecure);
+            var client = new InterNodeCommunication.InterNodeCommunicationClient(channel);
+
+            var reply = client.ExecuteRequestV1(new RequestV1 { Text = "T | count", Database = "NetDefaultDB", Properties = "properties" });
+            Console.WriteLine("Reply: " + reply.KustoDataStream.ToStringUtf8());
+
+            channel.ShutdownAsync().Wait();
+        }
+    }
+}
---
+++ service/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Grpc.Core;
+using Contract;
+
+namespace service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new Server
+            {
+                Services = { InterNodeCommunication.BindService(new InterNodeCommunicationService()) },
+                Ports = { new ServerPort ("localhost", 0, ServerCredentials.Insecure ) }
+            };
+
+            server.Start();
+            Console.WriteLine($"Bound port={server.Ports.First().BoundPort}");
+            Console.WriteLine("Waiting for you...");
+            Console.ReadKey();
+            server.ShutdownAsync().Wait();
+        }
+    }
+
+    internal class InterNodeCommunicationService : InterNodeCommunication.InterNodeCommunicationBase
+    {
+        public override Task<ReplyV1> ExecuteRequestV1(RequestV1 request, ServerCallContext context)
+        {
+            Console.WriteLine("Incoming request");
+            // throw new RpcException(new grpc::Status(grpc::StatusCode.Unimplemented, ""));
+            var kds = "Hello, Mister!";
+            return Task.FromResult(new ReplyV1 { KustoDataStream = Google.Protobuf.ByteString.CopyFromUtf8(kds) });
+        }
+    }
+}
